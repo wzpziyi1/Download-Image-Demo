3e853e38f6fa0530fb1d2317769e3026aafac3ee
wzpziyi1 <1204473090@qq.com>
2015-11-03 11:19:52 +0800

    Initial Commit

---
+++ 下载图片操作/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  下载图片操作
+//
+//  Created by 王志盼 on 15/11/3.
+//  Copyright © 2015年 王志盼. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
