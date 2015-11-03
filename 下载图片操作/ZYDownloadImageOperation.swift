d8dd9feae2b14889773d585d46ea50375369cfd4
wzpziyi1 <1204473090@qq.com>
2015-11-03 17:15:07 +0800

    first commit

---
+++ 下载图片操作/ZYDownloadImageOperation.swift
@@ -0,0 +1,13 @@
+//
+//  ZYDownloadImageOperation.swift
+//  下载图片操作
+//
+//  Created by 王志盼 on 15/11/3.
+//  Copyright © 2015年 王志盼. All rights reserved.
+//
+
+import UIKit
+
+class ZYDownloadImageOperation: NSOperation {
+
+}
